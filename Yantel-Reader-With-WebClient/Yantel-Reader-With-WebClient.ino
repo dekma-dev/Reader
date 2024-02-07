0743cb29e0e73cd684edc289e0c38342ce31fb45
dekma-dev <128292224+dekma-dev@users.noreply.github.com>
2024-02-07 15:50:16 +0300

    beta 1.1
    patch note with file moving

--- Yantel-Reader-With-WebClient/Yantel-Reader-With-WebClient.ino
+++ Yantel-Reader-With-WebClient/Yantel-Reader-With-WebClient.ino
@@ -7,10 +7,8 @@ unsigned long sending; //sends data to the server
 const char* ssid = "Patriarche Damir"; //own network's SSID
 const char* password = "snrk6276"; //and the password
 
-const char* host = "192.168.199.208"; //server's ip-address
-const int httpPort = 80; //check in the server's settings
-
-String url = "http://127.0.0.1:8000";
+const char* host = "192.168.53.208"; //server's ip-address
+const int httpPort = 8000; //check in the server's settings
 
 WiFiClient client;
 
@@ -39,15 +37,13 @@ void setup() {
 }
 
 void loop() {
-  delay(2000); //needed to avoid microcontroller overload 
+  delay(10000); //needed to avoid microcontroller overload 
 
   if (!client.connect(host, httpPort)) { //try to move to the setup
     Serial.println("Connection to server failed.");
     return;
   }
 
-  Serial.print("Requesting URL: "); //may be deleted
-  Serial.println(url); //too
   int id_stanok = random(1, 3); //random data needed for request
   int count = 0; //random data needed for request; count of closure
 
@@ -69,7 +65,7 @@ void loop() {
     }
     // delay(50); //for avoid wrong reading
     if (i == 24) { //вывод метки, где 8 - "помеха", а 6 последних бита - биты четности скорее всего. 
-                    //коэффициент в условии, например, 24 влияет на скорость вывода метки, влияение на дальность сомнительно.
+                   //коэффициент в условии, например, 24 влияет на скорость вывода метки, влияение на дальность сомнительно.
       for (byte a = 9; a < 19; a++) {
         Serial.print(bts[a], HEX); //convertation can be deleted.
         Serial.print("|");
@@ -81,26 +77,26 @@ void loop() {
   }
 
     //url = "http://127.0.0.1:8000/monitoring/sending/?ID_stanok=" + (String)id_stanok + "&RFID=" + RFID + "&Count=" + (String)count + "&State=" + (String)state + "&Purpose=" + (String)purpose + "&Country=" + (String)country;
-
-    url = "http://127.0.0.1:8000/monitoring/sending/?ID_stanok=124&RFID=SF234AS&Count=" + (String)count + "&State=1&Purpose=wqewqe&Country=qweqwe";
     
-    // if ((uint8_t)(millis() - sending) > 4000) { //idk why its doesnt work
-    //   sending = millis();
-          // client.print(String("GET ") + url + "Connection: close\r\n\r\n"); //sending request to the server
-      client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Connection: close\r\n\r\n"); //sending request to the server
-    // }
+  if ((uint8_t)(millis() - sending) > 4000) { //idk why its doesnt work
+    sending = millis();
+    Serial.println("sending request...");
+    
+    String url = "/monitoring/sending/?ID_stanok=124&RFID=SF234AS&Count=232234&State=1&Purpose=wqewqe&Country=qweqwe";
 
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-      if (millis() - timeout > 10000) {
-        Serial.println(">>> Client Timeout !");
-        client.stop();
-        return;
-      }
+    if (client.connected()) { 
+      client.printf("GET" + url + "HTTP/1.1\r\nHost: 192.168.53.208\r\nConnection: close\r\n\r\n");  //sending request to the server
     }
+  }
 
-  // Serial.println("\n\nThe ID: " + (String)id_stanok + " and the datas: " + RFID + " " + (String)count);
-  // Serial.println("request: ?id_stanok=" + (String)id_stanok + "&RFID=" + RFID + "&count=" + (String)count + "\n\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 10000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
 
   Serial.println("Closing connection.");
 
@@ -109,6 +105,7 @@ void loop() {
     sendQuery(bytePower, sizeof(bytePower));
   } 
 }
+
 
 void sendQuery(byte bytes[], byte len) { 
   Serial.println(len);
