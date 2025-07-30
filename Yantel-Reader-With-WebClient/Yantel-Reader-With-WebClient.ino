c2abbfb7654a144de1dfd9294bc9e0d869d3de4f
= <dekma_d@mail.ru>
2025-07-30 14:42:44 +0300

    release 3.0
    refactored

--- Yantel-Reader-With-WebClient/Yantel-Reader-With-WebClient.ino
+++ Yantel-Reader-With-WebClient/Yantel-Reader-With-WebClient.ino
@@ -1,47 +1,43 @@
-#include <WiFi.h> // necessary lib
+#include <WiFi.h>
 #include <HTTPClient.h>
-
-/*
-  Documentation
-  
-  1. All Serial print's may be deleted, cause they are needed only for debugging.
-  2. Reader have to have full-time working. Means that reading should not have delays.
-  3. Client should not spam the server with requests, delays are needed here.
-  4. For some reason asynchronous method yield() doesn't work in ESP32.
-  5. Needs 5V voltage power supply.
-  6. Available to add http-requests within authorization, it's can be useful if we want to filter access through that way penetration.
-
-*/
 
 byte bytePower[] = { 0xBB, 0x00, 0x27, 0x00, 0x03, 0x22, 0x27, 0x10, 0x83, 0x7E }; //power bytes code, needed for awake reader.
 
 unsigned long awaking; //awakes the reader.
 unsigned long sending; //sends data to the server
-unsigned long buttonTimer; //helps avoid reading interference
 unsigned long reading; //timer for choppy reading
 unsigned long reloadConnect; //reload the microcontroller system for re-connect to network
 unsigned long worktimeTimer; //needs for mark's work time calculation 
+unsigned long greenIndicatorTimer;
 
-const char* ssid = "Patriarche"; //own network's SSID
-const char* password = "snrk727776"; //and the password
+const char* ssid = "Patriarche";
+const char* password = "snrk727776";
 const char* serverName = "http://192.168.198.208:80/monitoring/sending";
 
-const char* host = "192.168.198.208"; //server's ip-address
-const int httpPort = 80; //check in the server's settings
+const char* host = "192.168.198.208";
+const int httpPort = 80;
 
-struct Button {
-  const uint8_t pin;
-  uint32_t closingCount;
-  bool pressed;
-};
-
-Button closingButton = {18, 1, false};
 String request = "", currentMark = "", leftMark = "";
+int closingButton = 18; //отдельная линия питания. разьем fakra
 unsigned long worktime = 0;
+uint32_t closingCount = 0;
 WiFiClient client;
 
+//Condition sign
+int greenLED = 4, yellowLED = 19, redLED = 21;
+/*HTTP response code:
+ -1/A - waiting / connected to WiFi, but HTTP requests are unsuccessful
+ 200 - typical OK statement
+ 408 - no WiFi connection 
+*/
+int httpResponseCode = -1;
+
 void setup() {
-  pinMode(closingButton.pin, INPUT_PULLUP);
+  pinMode(greenLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+
+  pinMode(closingButton, INPUT);
   pinMode(23, OUTPUT);
   digitalWrite(23, HIGH);
 
@@ -49,45 +45,51 @@ void setup() {
   Serial2.begin(115200); //COM-port needed for communications with reader
   delay(10);
 
-  Serial.print("\nConnecting to: ");
-  Serial.println(ssid);
-
   WiFi.begin(ssid, password); //connecting to local network
 
   while (WiFi.status() != WL_CONNECTED) { //checking connection
-    Serial.print(".");
+    httpResponseCode = 408;
+    digitalWrite(redLED, HIGH);
     delay(1000); 
 
     if (millis() - reloadConnect > 10000) { 
       WiFi.disconnect();
-      Serial.println("Connecting...");
       WiFi.begin(ssid, password, 0, NULL, false);
       WiFi.reconnect();
       
       reloadConnect = millis();
     }
   }
-  Serial.println();
-  
-  Serial.print("This is the connected user: ");
-  Serial.println(WiFi.localIP());
-  
-  Serial.print("Connecting to: ");
-  Serial.println(host);
+
+  greenIndicatorTimer = millis();
   
   sendQuery(bytePower, sizeof(bytePower)); //reader awaking
-
-  attachInterrupt(closingButton.pin, calculateClosing, HIGH);
 }
 
 void loop() {
-  while (WiFi.status() != WL_CONNECTED) { //checking connection
-    Serial.print(".");
+  if (WiFi.status() != WL_CONNECTED) httpResponseCode = 408;
+  else if (httpResponseCode != 200) httpResponseCode = -1;
+
+  if (httpResponseCode == 408) {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, HIGH);
+  } else if (httpResponseCode == 200 && (millis() - greenIndicatorTimer > 250)) {
+    digitalWrite(greenLED, digitalRead(greenLED ) ^ 1);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, LOW);
+    greenIndicatorTimer = millis();
+  } else if (httpResponseCode == -1) {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+    digitalWrite(redLED, LOW);
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000); 
 
     if (millis() - reloadConnect > 10000) { 
       WiFi.disconnect();
-      Serial.println("Connecting...");
       WiFi.begin(ssid, password, 0, NULL, false);
       WiFi.reconnect();
       
@@ -103,11 +105,9 @@ void loop() {
     else worktime = 0;
   }
 
-  // request = "GET /monitoring/sending/?RFID=";
   request = "?RFID=";
   leftMark = currentMark;
 
-  // if (millis() - reading > 5000) {
     if (Serial2.available()) {
       byte bts[256]; //256 - too  few (mem overflow), 1024 - too slow reading.
       byte i = 0;
@@ -120,40 +120,28 @@ void loop() {
             Serial2.read();
             delay(10);
             Serial.println("Overload");
-          } 
+          }
         }
       }
-      // delay(50);
-      if (i == 24) { //вывод метки, где 8 - "помеха", а 6 последних бита - биты четности скорее всего. 
-                    //коэффициент в условии, например, 24 влияет на скорость вывода метки, влияение на дальность сомнительно.
         for (byte a = 9, index = 0; a < 19; a++, index++) {
           request += String(bts[a], HEX);
           currentMark += String(bts[a], HEX);
         }
 
-        if (millis() - sending > 60000) {
+        if (millis() - sending > 29900) {
           sending = millis(); 
-          // sendGetHttpRequest(request);
           sendGETtHttpRequest(request);
         }
       }
     }       
-  //probably should be the same that up there
+
   if (millis() - awaking > 70000) {
-    awaking = millis();   
+    awaking = millis();  
     sendQuery(bytePower, sizeof(bytePower));
   }
 
-  if (closingButton.pressed && !digitalRead(closingButton.pin) && millis() - buttonTimer > 50) {
-    closingButton.pressed = false;
-    buttonTimer = millis();
-  }
-}
-
-void calculateClosing() {
-  if (!closingButton.pressed && millis() - buttonTimer > 50) {
-    closingButton.closingCount++;
-    closingButton.pressed = true;
+  if (millis() - buttonTimer > 50) {
+    closingCount++;
     buttonTimer = millis();
   }
 }
@@ -165,84 +153,25 @@ void sendGETtHttpRequest(String request) {
     int id_stanok = random(1, 10); //random data needed for request
 
     request += "&ID_stanok=" + String(id_stanok);
-    request += "&Count=" + String(closingButton.closingCount);
+    request += "&Count=" + String(closingCount);
     request += "&WorkTime=" + String(worktime);
     request += "&State=Установлена&Purpose=None&Country=None HTTP/1.1\r\nHost: 192.168.198.208\r\nConnection: close\r\n\r\n"; //State должно быть строкой, как в бд
-
-    Serial.println("sending request...");
-    Serial.println(request);
 
     String serverPath = serverName + request;
     
     http.begin(serverPath.c_str());
     
-    // If you need Node-RED/server authentication, insert user and password below
-    //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-    
-    int httpResponseCode = http.GET();
+    httpResponseCode = http.GET();
       
-    //Дебаг отправки запроса
-
-    // if (httpResponseCode > 0) {
-    //   Serial.print("HTTP Response code: ");
-    //   Serial.println(httpResponseCode);
-    //   String payload = http.getString();
-    //   Serial.println(payload);
-    // } else {
-    //   Serial.print("Error code: ");
-    //   Serial.println(httpResponseCode);
-    // }
     http.end();
 
-    closingButton.closingCount = 1;
+    closingCount = 1;
 
   } else  Serial.println("WiFi Disconnected");
 }
 
-//Старый GET запрос, сейчас не актуален - оставлен на всякий случай
-
-// void sendGetHttpRequest(String request) {
-//   Serial.printf("This is mark's working time: %d", worktime);
-
-//   if (!client.connect(host, httpPort)) { //try to move to the setup
-//     Serial.println("Connection to server failed.");
-//     return;
-//   }
-
-//   int id_stanok = random(1, 10); //random data needed for request
-
-//   request += "&ID_stanok=" + String(id_stanok);
-//   request += "&Count=" + String(closingButton.closingCount);
-//   request += "&WorkTime=" + String(worktime);
-//   request += "&State=Установлена&Purpose=None&Country=None HTTP/1.1\r\nHost: 192.168.198.208\r\nConnection: close\r\n\r\n"; //State должно быть строкой, как в бд
-
-//   Serial.println("sending request...");
-//   Serial.println(request);
-
-
-//   if (client.connected()) { 
-//     client.print(request);  //sending request to the server
-//   }
-
-//   closingButton.closingCount = 1;
- 
-//   Serial.println("Closing connection.");
-
-//   unsigned long timeout = millis();
-//   while (client.available() == 0) {
-//     if (millis() - timeout > 10000) {
-//       Serial.println(">>> Client Timeout !");
-//       client.stop();
-//       return;
-//     }
-//   }
-// }
-
 void sendQuery(byte bytes[], byte len) { 
-  Serial.println(len);
   for (byte i = 0; i < len; i++) Serial2.write(bytes[i]);
   for (byte i = 0; i < len; i++) { //just for debugging
-    Serial.print(bytes[i], HEX);
-    Serial.print('\t');
   }
 }
